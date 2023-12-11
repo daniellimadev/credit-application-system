ea2f665a7e69e9765019f4991e6afd687a256fc7
daniellimadev <pdaniel643@gmail.com>
2023-12-11 18:56:41 -0300

    Creating part of Services and Exception

---
+++ src/main/kotlin/com/daniel/credit/application/system/service/ICustomerService.kt
@@ -0,0 +1,9 @@
+package com.daniel.credit.application.system.service
+
+import com.daniel.credit.application.system.entity.Customer
+
+interface ICustomerService {
+    fun save(customer: Customer): Customer
+    fun findById(id: Long): Customer
+    fun delete(id: Long)
+}
